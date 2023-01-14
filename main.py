5052fbf983e8600fcfbb6797c9533d60097a5076
sarboleda72 <sarboleda72@misena.edu.co>
2023-01-14 18:42:11 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from persona import Persona
+
+persona1=Persona()
+persona1.nombre="Santiago"
+persona1.edad=26
+persona1.mostrar_datos()
---
+++ persona.py
@@ -0,0 +1,7 @@
+class Persona:
+    nombre=None
+    edad=None
+    def mostrar_datos(self):
+        print("Nombre: ",self.nombre)
+        print("Edad: ",self.edad)
+        
